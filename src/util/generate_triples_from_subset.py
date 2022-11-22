cc6cf8cc90572b0236e360a13b9d60a88368a41f
Andrew <2393265p@student.gla.ac.uk>
2022-11-22 15:43:28 +0000

    Added triples filter

--- src/util/generate_triples_from_subset.py
+++ src/util/generate_triples_from_subset.py
@@ -1,13 +1,12 @@
 import argparse
-from math import trunc 
-import random
 import pandas as pd
 import numpy as np
+import ir_datasets
 
 parser = argparse.ArgumentParser(description='Build Subset of IR Dataset in the form of <qid, docid_a, docid_b>')
 
 parser.add_argument('-source', type=str, default=None, help='Dataset Source')
-parser.add_argument('-portion', type=int, default=None, nargs='+', help=r'% of dataset to use')
+parser.add_argument('-subset', type=int, default=None, help='Query file containing ids')
 parser.add_argument('-suffix', type=str, default='train', help='Suffix of output name')
 parser.add_argument('-out', type=str, help='Output dir for tsv')
 
@@ -16,13 +15,11 @@ def main(args):
     types = {col : np.int32 for col in cols}
     with open(args.source, 'r') as f:
         df = pd.read_csv(f, sep='\t', header=None, index=False, names=cols, dtype=types)
+    dataset = ir_datasets.load(args.subset)
+    filter = df['qid'].isin([query.qid for query in dataset.queries_iter()])
 
-    for portion in args.portion:
-        num_choices = trunc(len(df) * (args.portion/100))
-        sub_df = df.sample(n=num_choices)
+    filtered_df = df[filter]
+    filtered_df.to_csv(args.out + f'triples.{args.suffix}.tsv', sep='\t', index=False, header=False)
 
-        sub_df.to_csv(args.out + f'triples.{args.suffix + str(portion)}.tsv', sep='\t', index=False, header=False)
-
-
-if __name__ -- '__main__':
+if __name__ == '__main__':
     main(parser.parse_args())
