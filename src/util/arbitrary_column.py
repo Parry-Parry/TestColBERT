31c8b3b92c8439ada7e3455a58d49bb8e19b49f2
Andrew Parry <2393265p@student.gla.ac.uk>
2022-11-23 22:05:15 +0000

    Adding silly utils

---
+++ src/util/arbitrary_column.py
@@ -0,0 +1,19 @@
+import argparse
+import pandas as pd
+import numpy as np
+
+parser = argparse.ArgumentParser(description='New Col')
+
+parser.add_argument('-source', type=str, default=None, help='Dataset Source')
+parser.add_argument('-out', type=str, help='Output dir for New file')
+
+def main(args):
+    cols = ['qid', 'docid_a', 'doc_id_b']
+    types = {col : np.int32 for col in cols}
+    with open(args.source, 'r') as f:
+        df = pd.read_csv(f, sep='\t', header=None, index_col=False, names=cols, dtype=types)
+    df['arbitrary'] = str(0)
+    df.to_json(args.out, orient='records', lines=True)
+
+if __name__ == '__main__':
+    main(parser.parse_args())
